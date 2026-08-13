498344f2b3e8af086d4168cd8dded85bfdad9037
shubh jain <sjain21012007@gmail.com>
2026-08-13 17:20:08 +0530

    Time: 741 ms (5.07%) | Memory: 431.1 MB (5.39%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void generate(int i , vector<vector<int>> & ans,vector<int>& candidates, int target,vector<int> curr,int sum,map<vector<int>,int>& m)
+    {
+        if(sum==target)
+        {
+            if(m[curr]==0)
+            {
+                ans.push_back(curr);
+                m[curr]++;
+            }
+            return;
+        }
+        else if(i== candidates.size() || sum>target)
+        {
+            return ;
+        }
+        else{
+            generate(i+1,ans,candidates,target,curr,sum,m);
+            sum+=candidates[i];
+            curr.push_back(candidates[i]);
+            generate(i,ans,candidates,target,curr,sum,m);
+            generate(i+1,ans,candidates,target,curr,sum,m);
+
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<vector<int>> ans;
+       vector<int> curr;
+       map<vector<int>,int> m;
+       int sum =0;
+       generate(0,ans,candidates,target,curr,sum,m);
+       return ans;
+    }
+};
