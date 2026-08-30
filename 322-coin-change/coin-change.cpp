f4c7beba64a957403a23ac1915fc77e7ac12518a
shubh jain <sjain21012007@gmail.com>
2026-08-30 15:56:20 +0530

    Time: 46 ms (37.60%) | Memory: 52.9 MB (16.01%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,6 +5,10 @@ public:
         a.push_back(0);
         sort(a.begin(),a.end());
         vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
+        for(int i =0;i<=amount;i++)
+        {
+            dp[0][i]= 1e9;
+        }
         for(int i=0;i<n+1;i++)
         {
             dp[i][0]=0;
@@ -13,28 +17,13 @@ public:
         {
             for(int price=1;price<=amount;price++)
             {
-                int ans = dp[i-1][price];
-                if(price<a[i]) dp[i][price]=ans;
-                else 
-                {
-                    if(price%a[i]==0)
-                    {
-                       if(ans==-1) dp[i][price]=price/a[i];
-                       else dp[i][price]=min(ans,price/a[i]);
-                    }
-                    for(int factor=1;factor<=price/a[i];factor++)
-                    {
-                        if(dp[i-1][price-factor*a[i]]==-1) continue;
-                        else
-                        {
-                           if (ans==-1) ans = dp[i-1][price - factor * a[i]] + factor;
-                            else ans=min(ans, dp[i-1][price - factor * a[i]] + factor);
-                        }
-                    } 
-                }
-                dp[i][price] = ans; 
+               int notake= dp[i-1][price];
+               int take = INT_MAX;
+               if(price>=a[i])  take = 1+dp[i][price-a[i]];
+               dp[i][price] = min(notake,take);
             }
         }
+        if(dp[n][amount]>=1e9) return -1;
         return dp[n][amount];
     }      
 };
