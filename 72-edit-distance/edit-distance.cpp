fecb0bc52acdb3df17f2684c8aa0b9a709a0fe69
shubh jain <sjain21012007@gmail.com>
2026-09-17 17:13:23 +0530

    Time: 4 ms (81.58%) | Memory: 13.7 MB (24.48%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,25 +4,24 @@ public:
         int n = s.length();
         int m = t.length();
         vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        for(int i=1;i<=n;i++)
+        vector<int> curr(m+1,0), prev(m+1,0);
+        for(int i=1;i<=m;i++)
         {
-            dp[i][0]=i;
-        }
-         for(int i=1;i<=m;i++)
-        {
-            dp[0][i]=i;
+            prev[i]=i;
         }
         for(int i=1;i<=n;i++)
         {
+            curr[0]=i;
             for(int j=1;j<=m;j++)
             {
-                if(s[i-1]==t[j-1])  dp[i][j] = dp[i-1][j-1];
+                if(s[i-1]==t[j-1])  curr[j] = prev[j-1];
                 else 
                 {
-                   dp[i][j] = 1+ min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                   curr[j] = 1+ min(prev[j-1],min(prev[j],curr[j-1]));
                 } 
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
