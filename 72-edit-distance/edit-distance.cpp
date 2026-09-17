89b79d715026feadb50a192fd62d602d939e6cd5
shubh jain <sjain21012007@gmail.com>
2026-09-17 17:07:27 +0530

    Time: 3 ms (91.58%) | Memory: 13.2 MB (61.27%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -11,10 +11,29 @@ public:
             return dp[i][j] = 1+ min(f(i-1,j-1,s,t,dp),min(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp)));
         }
     }
-    int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return f(n,m,word1,word2,dp);
+    int minDistance(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            dp[i][0]=i;
+        }
+         for(int i=1;i<=m;i++)
+        {
+            dp[0][i]=i;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])  dp[i][j] = dp[i-1][j-1];
+                else 
+                {
+                    dp[i][j] = 1+ min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                 } 
+            }
+        }
+        return dp[n][m];
     }
 };
